dff0fd77bcabceed619c624184a49c735a119640
Qingyue Wang <qingyuewang@QingyueMacBook-Pro-M3.local>
2026-07-25 02:22:15 +0800

    Lower the L1-B3 transport corridor

--- experiments/robot/libero/tasks/run_l1b_swept.sh
+++ experiments/robot/libero/tasks/run_l1b_swept.sh
@@ -260,7 +260,7 @@ safe_reference_family() {
     extra_args+=(--max_waypoint_steps 400 --transport_max_waypoint_steps 700)
     extra_args+=(--transport_max_position_command 0.12)
     extra_args+=(--position_tolerance 0.025 --transport_position_tolerance 0.012)
-    extra_args+=(--transport_clearance 0.01)
+    extra_args+=(--transport_clearance 0.00)
     extra_args+=(--transport_target_eef_quat 0.9941969,-0.0504397,-0.0834422,0.0454506)
     # First carry the bowl along the safe side of the corridor. Only reorient
     # after leaving the bottle, and rotate slowly enough to retain a rim grasp.
@@ -281,7 +281,7 @@ safe_reference_family() {
     extra_args+=(--transport_obstacle_clearance 0.001)
     extra_args+=(--transport_obstacle_segments 8)
     extra_args+=(--transport_arc_position_tolerance 0.032)
-    extra_args+=(--preplace_height 0.04 --max_safe_lift_height 0.09)
+    extra_args+=(--preplace_height 0.02 --max_safe_lift_height 0.09)
     extra_args+=(--place_offset_x 0.00 --place_offset_y 0.015)
     extra_args+=(--require_support_contact_before_release)
     extra_args+=(--support_contact_hold_steps 10)
