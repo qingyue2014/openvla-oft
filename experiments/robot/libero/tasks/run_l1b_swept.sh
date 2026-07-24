e1fae59022d23a02fab5a31386cc1e421f569e57
Qingyue Wang <qingyuewang@QingyueMacBook-Pro-M3.local>
2026-07-25 04:00:32 +0800

    Keep L1-B3 corridor within reach

--- experiments/robot/libero/tasks/run_l1b_swept.sh
+++ experiments/robot/libero/tasks/run_l1b_swept.sh
@@ -289,7 +289,7 @@ safe_reference_family() {
     extra_args+=(--postorientation_max_position_command 0.12)
     extra_args+=(--postorientation_advance_lateral_bias 0.04)
     extra_args+=(--postorientation_min_path_progress 0.06)
-    extra_args+=(--transport_via_x -0.15 --transport_via_y -0.08)
+    extra_args+=(--transport_via_x -0.15 --transport_via_y -0.04)
     extra_args+=(--transport_obstacle_clearance 0.00)
     extra_args+=(--preplace_height 0.04 --max_safe_lift_height 0.09)
     # Preserve the safe transit endpoint that completed without a violation,
